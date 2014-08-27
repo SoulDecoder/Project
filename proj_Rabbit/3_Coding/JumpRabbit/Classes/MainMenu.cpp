0ab0fa31947048e605f5fa664d2bbb2cc07c5a0f
gitconfig <souldecoder@sina.com>
2014-08-27 22:17:19 +0800

    修改了Hellowolrd.png
    修改了Hellowolrd.png

--- proj_Rabbit/3_Coding/JumpRabbit/Classes/MainMenu.cpp
+++ proj_Rabbit/3_Coding/JumpRabbit/Classes/MainMenu.cpp
@@ -73,7 +73,7 @@ bool MainMenu::init()
     this->addChild(label, 1);
     
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("CloseSelected.png");
     
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
